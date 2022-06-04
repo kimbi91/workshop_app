023dc094b0c6fe40a987f367d26a82d8a10e1238
kimbi91 <subscribefan@gmail.com>
2022-06-04 12:03:28 +0200

    TyreSet class created

---
+++ src/hu/progmatic/models/TyreSet.java
@@ -0,0 +1,81 @@
+package hu.progmatic.models;
+
+public class TyreSet {
+
+    private Client owner;
+    private boolean isWheel;
+    private String brand;
+    private int treadDepht;
+    private boolean isSummerTread;
+    private int numOfTyresInSet;
+    private String damages;
+
+    public TyreSet() {
+    }
+
+    public TyreSet(Client owner, boolean isWheel, String brand, int treadDepht, boolean isSummerTread, int numOfTyresInSet, String damages) {
+        this.owner = owner;
+        this.isWheel = isWheel;
+        this.brand = brand;
+        this.treadDepht = treadDepht;
+        this.isSummerTread = isSummerTread;
+        this.numOfTyresInSet = numOfTyresInSet;
+        this.damages = damages;
+    }
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
+
+    public boolean isWheel() {
+        return isWheel;
+    }
+
+    public void setWheel(boolean wheel) {
+        isWheel = wheel;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getTreadDepht() {
+        return treadDepht;
+    }
+
+    public void setTreadDepht(int treadDepht) {
+        this.treadDepht = treadDepht;
+    }
+
+    public boolean isSummerTread() {
+        return isSummerTread;
+    }
+
+    public void setSummerTread(boolean summerTread) {
+        isSummerTread = summerTread;
+    }
+
+    public int getNumOfTyresInSet() {
+        return numOfTyresInSet;
+    }
+
+    public void setNumOfTyresInSet(int numOfTyresInSet) {
+        this.numOfTyresInSet = numOfTyresInSet;
+    }
+
+    public String getDamages() {
+        return damages;
+    }
+
+    public void setDamages(String damages) {
+        this.damages = damages;
+    }
+}
